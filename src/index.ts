238566e0efd94101ee334022c5a57f55b00da82f
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-09-23 16:35:22 -0300

    replace index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-export { Button } from "./components/Button";
-export type { ButtonProps } from "./components/Button";
-
-export { Badge } from "./components/Badge";
-export type { TVariant } from "./components/Badge";
-
-export { Dropdown } from "./components/Dropdown";
+export * from "./components";
